fea289d680cb0ee026f13f762ad27280ec802bbb
sharccc <109774053+sharccc@users.noreply.github.com>
2024-03-27 14:59:14 +1100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -6,9 +6,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.11, < 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">=3.0.0"
-    }
+    #random = {
+    #  source  = "hashicorp/random"
+    #  version = ">=3.0.0"
+    #}
   }
 }
